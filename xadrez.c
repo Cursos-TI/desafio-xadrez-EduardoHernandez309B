32af7902db9157f5df97f725ad7762a39956dba5
EDUARDO MACHADO HERNANDEZ <edu812hernandez@gmail.com>
2025-10-07 23:33:46 +0000

    Adição de loop aninhado para movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,37 +5,56 @@ int main()
     // ----------------------------
     // Torre - usando FOR
     // ----------------------------
-    int casasTorre = 5; //número de casa que a torre vai andar
+    int casasTorre = 5; // número de casa que a torre vai andar
     printf("Movimento da Torre:\n");
-    for (int i = 1; i <= casasTorre; i++) {
+    for (int i = 1; i <= casasTorre; i++)
+    {
         printf("Direita\n");
     }
 
     // ----------------------------
     // Bispo - usando WHILE
     // ----------------------------
-    int casasBispo = 5; //número de casas que o bispo vai andar
+    int casasBispo = 5; // número de casas que o bispo vai andar
     int j = 1;
     printf("\nMovimento do Bispo:\n");
-    while (j <= casasBispo) {
+    while (j <= casasBispo)
+    {
         printf("Cima, Direita\n");
         j++;
     }
-    
+
     // ----------------------------
     // Rainha - usando DO...WHILE
     // ----------------------------
-    int casasRainha = 8; //número de casas que a Rainha vai andar
+    int casasRainha = 8; // número de casas que a Rainha vai andar
     int k = 1;
     printf("\nMovimento da Rainha\n");
     do
     {
-       printf("Esquerda\n");
-       k++;
+        printf("Esquerda\n");
+        k++;
     } while (k <= casasRainha);
-    
-    
 
+    // ----------------------------
+    // Cavalo - usando loops aninhados
+    // ----------------------------
+
+    int casasBaixo = 2;
+    int casasEsquerda = 1;
+    printf("\nMovimento do Cavalo\n");
+    // Loop principal para o movimento para baixo
+    for (int i = 1; i <= casasBaixo; i++)
+    {
+        printf("Baixo\n");
+
+        // Loop aninhado para o movimento perpendicular
+        int l = 1;
+        while(i == casasBaixo && l<= casasEsquerda) {
+            printf("Esquerda\n");
+            l++;
+        }
+    }
 
     return 0;
 }
